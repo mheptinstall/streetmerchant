d1ddce814f2033ea3a6c723510555df3c637081d
Mark Heptinstall <mheptinstall@gmail.com>
2021-01-31 10:20:04 +0000

    Update ShopTo

--- src/store/model/shopto.ts
+++ src/store/model/shopto.ts
@@ -4,9 +4,13 @@ export const ShopTo: Store = {
   currency: '£',
   labels: {
     inStock: {
-      container: '.orderbox_inventory',
-      text: ['In Stock'],
+      container: '#itemcard_right .orderbox_inventory',
+      text: ['In Stock', 'Back Order'],
     },
+    outOfStock: {
+      container: '#itemcard_right .orderbox_inventory',
+      text: ['Sold out']
+    }
   },
   links: [
     {
@@ -14,7 +18,21 @@ export const ShopTo: Store = {
       model: 'test:model',
       series: 'test:series',
       url:
-        'https://www.shopto.net/en/ps5du00-dualsense-controller-playstation-5-p195100/',
+        'https://www.shopto.net/en/ps5de02-demon-souls-playstation-5-p195338/',
+    },
+    {
+      brand: 'test:brand',
+      model: 'test:model',
+      series: 'test:series',
+      url:
+        'https://www.shopto.net/en/ps5ac01-pulse-3d-wireless-headset-playstation-5-p195330/',
+    },
+    {
+      brand: 'test:brand',
+      model: 'test:model',
+      series: 'test:series',
+      url:
+        'https://www.shopto.net/en/ps5im07-immortals-fenyx-rising-gold-edition-p502367/',
     },
     {
       brand: 'sony',
@@ -29,6 +47,12 @@ export const ShopTo: Store = {
       url:
         'https://www.shopto.net/en/ps5hw02-playstation-5-digital-console-p195341/',
     },
+    {
+      brand: 'microsoft',
+      model: 'xbox series x',
+      series: 'xboxsx',
+      url: 'https://www.shopto.net/en/xbxhw01-xbox-series-x-p191471/'
+    }
   ],
   name: 'shopto',
 };
